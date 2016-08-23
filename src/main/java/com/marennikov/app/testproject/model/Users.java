4a2b618254c76d0cd07e683486e9b97051822d51
Godhil <dgodhil@gmail.com>
2016-08-23 15:16:05 +0300

    Add Entity

---
+++ src/main/java/com/marennikov/app/testproject/model/Users.java
@@ -0,0 +1,104 @@
+package com.marennikov.app.testproject.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private Integer password;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @Column(name = "role")
+    private Integer role;
+// принадлежность к муниципальному образованию
+//    @Column(name = "")
+//    private Integer municipalityId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Integer getPassword() {
+        return password;
+    }
+
+    public void setPassword(Integer password) {
+        this.password = password;
+    }
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(Integer role) {
+        this.role = role;
+    }
+
+//    public Integer getMunicipalityId() {
+//        return municipalityId;
+//    }
+//
+//    public void setMunicipalityId(Integer municipalityId) {
+//        this.municipalityId = municipalityId;
+//    }
+
+    @Override
+    public String toString() {
+        return "User: { " + getSurname() + ", " + getFirstName() +
+                ", role: " + getRole()+ ", municipality: "  + "}";
+    }
+}
