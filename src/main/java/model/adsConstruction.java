67b746e739eae6f126db66c35678249c0c6b3d1b
Godhil <dgodhil@gmail.com>
2016-08-23 13:53:48 +0300

    fist commit

---
+++ src/main/java/model/adsConstruction.java
@@ -0,0 +1,7 @@
+package main.java.model;
+
+/**
+ * Created by denis on 23.08.16.
+ */
+public class adsConstruction {
+}
---
+++ src/main/java/testproject.java
@@ -0,0 +1,7 @@
+package main.java;
+
+public class testproject {
+    public static void main(String[] args) {
+
+    }
+}
