f020c808aae02710ccf00a9f3a0ca663c044b271
Godhil <dgodhil@gmail.com>
2016-08-26 11:38:38 +0300

    Add query for find photo by requests id

--- src/main/java/com/marennikov/app/testproject/repository/IPhotoRepository.java
+++ src/main/java/com/marennikov/app/testproject/repository/IPhotoRepository.java
@@ -2,6 +2,11 @@ package com.marennikov.app.testproject.repository;
 
 import com.marennikov.app.testproject.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface IPhotoRepository extends JpaRepository<Photo, Integer> {
+
+    @Query("from Photo where requests_id = :id")
+    Iterable<Photo> findByRequestId(@Param("id") Integer id);
 }
