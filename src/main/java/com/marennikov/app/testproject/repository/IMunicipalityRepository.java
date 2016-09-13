1a00efd7797925ad4263f199d5d53a28c551a9c4
Godhil <dgodhil@gmail.com>
2016-09-13 23:50:03 +0300

    Create findAllByActiveStatus method

--- src/main/java/com/marennikov/app/testproject/repository/IMunicipalityRepository.java
+++ src/main/java/com/marennikov/app/testproject/repository/IMunicipalityRepository.java
@@ -2,21 +2,10 @@ package com.marennikov.app.testproject.repository;
 
 import com.marennikov.app.testproject.entity.Municipality;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface IMunicipalityRepository extends JpaRepository<Municipality, Integer> {
 
-//    @Modifying
-//    @Query("update Municipality mun " +
-//            "set mun.actualStatus = 'delete' " +
-//            "where mun.id =:id")
-//    void setStatus(@Param("id") Integer id);
-
     List<Municipality> findAllByActiveStatus(String activeStatus);
-
-
 }
