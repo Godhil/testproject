459997e20c4ae61c4dfd4394294a3024655cfab4
Godhil <dgodhil@gmail.com>
2016-09-14 00:06:22 +0300

    Create new web config for Thymeleaf

---
+++ src/main/java/com/marennikov/app/testproject/config/SpringWebConfig.java
@@ -0,0 +1,137 @@
+package com.marennikov.app.testproject.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.marennikov.app.testproject")
+public class SpringWebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private static final String CHARACTER_ENCODING = "UTF-8";
+
+
+    private ApplicationContext applicationContext;
+
+
+
+    public SpringWebConfig() {
+        super();
+    }
+
+
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+
+    /**
+     *  Message externalization/internationalization
+     */
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("Messages");
+        return resourceBundleMessageSource;
+    }
+
+    /* **************************************************************** */
+    /*  THYMELEAF-SPECIFIC ARTIFACTS                                    */
+    /*  TemplateResolver <- TemplateEngine <- ViewResolver              */
+    /* **************************************************************** */
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding(CHARACTER_ENCODING);
+        // Template cache is true by default. Set to false if you want
+        // templates to be automatically updated when modified.
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setEnableSpringELCompiler(true); // Compiled SpringEL should speed up executions
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setCharacterEncoding(CHARACTER_ENCODING);
+        viewResolver.setContentType("text/html;charset=UTF-8");
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    /* ******************************************************************* */
+    /*  Defines callback methods to customize the Java-based configuration */
+    /*  for Spring MVC enabled via {@code @EnableWebMvc}                   */
+    /* ******************************************************************* */
+
+    /**
+     *  Dispatcher configuration for serving static resources
+     */
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
+        registry.addResourceHandler("/WEB-INF/templates/**").addResourceLocations("/templates/");
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding(CHARACTER_ENCODING);
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+//
+//    /**
+//     * Add formatter for class {@link thymeleafexamples.stsm.business.entities.Variety}
+//     * and {@link java.util.Date} in addition to the one registered by default
+//     */
+//    @Override
+//    public void addFormatters(final FormatterRegistry registry) {
+//        super.addFormatters(registry);
+//        registry.addFormatter(varietyFormatter());
+//        registry.addFormatter(dataFormatter());
+//    }
+//
+//    @Bean
+//    public VarietyFormatter varietyFormatter() {
+//        return new VarietyFormatter();
+//    }
+//
+//    @Bean
+//    public DateFormatter dataFormatter() {
+//        return new DateFormatter();
+//    }
+
+}
