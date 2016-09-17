0c589982885e46c5068d3fdc3d84a2ead850a633
Godhil <dgodhil@gmail.com>
2016-09-17 18:44:46 +0300

    Fix unique municipality name post

--- src/main/java/com/marennikov/app/testproject/controller/MunicipalityController.java
+++ src/main/java/com/marennikov/app/testproject/controller/MunicipalityController.java
@@ -15,6 +15,10 @@ public class MunicipalityController {
 
     private IMunicipalityService municipalityService;
 
+    private Municipality itemById ;
+
+    private Municipality itemByName;
+
     @Autowired
     public void setMunicipalityService(IMunicipalityService municipalityService) {
         this.municipalityService = municipalityService;
@@ -43,14 +47,34 @@ public class MunicipalityController {
 
     //сохранение
     @RequestMapping(value = "/municipalities", method = RequestMethod.POST)
-    public String saveMunicipality(Municipality municipality){
-        municipalityService.saveMunicipality(municipality);
-        return "redirect:/municipality/municipalities";
+    public String saveMunicipality(Municipality municipality) {
+
+        itemByName = municipalityService.getByName(municipality.getName());
+
+        if (municipality.getId() != null) {
+
+            itemById = municipalityService.getById(municipality.getId());
+
+            if ((itemById.getName().equals(municipality.getName())) ||
+                    (itemByName == null)) {
+
+                municipalityService.saveMunicipality(municipality);
+                return "redirect:/municipality/municipalities";
+
+            } else return "redirect:/municipality/municipality/" + municipality.getId();
+
+        } else if (itemByName == null) {
+
+            municipalityService.saveMunicipality(municipality);
+            return "redirect:/municipality/municipalities";
+        }
+
+        return "redirect:/municipality/municipality";
     }
 
     //отключение(установить флаг "Delete")
     @RequestMapping(value = "/municipality/delete/{id}", method = RequestMethod.POST)
-    public String setDelete(@PathVariable Integer id){
+    public String setDelete(@PathVariable Integer id) {
         Municipality municipality = municipalityService.getById(id);
         municipalityService.setDeleteStatus(municipality);
         return "redirect:/municipality/municipalities";
