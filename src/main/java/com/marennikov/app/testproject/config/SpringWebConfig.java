beff20324600888e1a4b5eb38665102b9a5725d1
Godhil <dgodhil@gmail.com>
2016-09-18 15:07:38 +0300

    Remove encoding filter

--- src/main/java/com/marennikov/app/testproject/config/SpringWebConfig.java
+++ src/main/java/com/marennikov/app/testproject/config/SpringWebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -21,8 +20,6 @@ import org.thymeleaf.templatemode.TemplateMode;
 @EnableWebMvc
 @ComponentScan("com.marennikov.app.testproject")
 public class SpringWebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
-
-    private static final String CHARACTER_ENCODING = "UTF-8";
 
     private ApplicationContext applicationContext;
 
@@ -56,9 +53,6 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter implements Applicat
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setTemplateMode(TemplateMode.HTML);
-        templateResolver.setCharacterEncoding(CHARACTER_ENCODING);
-        // Template cache is true by default. Set to false if you want
-        // templates to be automatically updated when modified.
         templateResolver.setCacheable(true);
         return templateResolver;
     }
@@ -74,8 +68,7 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter implements Applicat
     @Bean
     public ThymeleafViewResolver viewResolver(){
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setCharacterEncoding(CHARACTER_ENCODING);
-        viewResolver.setContentType("text/html;charset=UTF-8");
+        viewResolver.setContentType("text/html;charset=utf-8");
         viewResolver.setTemplateEngine(templateEngine());
         return viewResolver;
     }
@@ -97,34 +90,4 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter implements Applicat
         registry.addResourceHandler("/fonts/**").addResourceLocations("/fonts/");
         registry.addResourceHandler("/WEB-INF/templates/**").addResourceLocations("/templates/");
     }
-
-    @Bean
-    public CharacterEncodingFilter characterEncodingFilter() {
-        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
-        characterEncodingFilter.setEncoding(CHARACTER_ENCODING);
-        characterEncodingFilter.setForceEncoding(true);
-        return characterEncodingFilter;
-    }
-//
-//    /**
-//     * Add formatter for class {@link thymeleafexamples.stsm.business.entities.Variety}
-//     * and {@link java.util.Date} in addition to the one registered by default
-//     */
-//    @Override
-//    public void addFormatters(final FormatterRegistry registry) {
-//        super.addFormatters(registry);
-//        registry.addFormatter(varietyFormatter());
-//        registry.addFormatter(dataFormatter());
-//    }
-//
-//    @Bean
-//    public VarietyFormatter varietyFormatter() {
-//        return new VarietyFormatter();
-//    }
-//
-//    @Bean
-//    public DateFormatter dataFormatter() {
-//        return new DateFormatter();
-//    }
-
 }
